a2a113b7de9e30d3afb1d16ee091c2002f0a62a3
jimolle <jimolle@users.noreply.github.com>
2016-03-02 13:56:50 +0100

    Initial Commit

---
+++ MVCMock/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCMock.Startup))]
+namespace MVCMock
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
